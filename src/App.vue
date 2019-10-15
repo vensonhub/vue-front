48851c245bc4a93a481c7c6788892bc4ee7ec1b6
jiajia <283747923@qq.com>
2019-10-16 02:18:13 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <ul>
+      <li v-for="(item,index) in users " :key="index">
+        {{item.name}}
+      </li>
+    </ul>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data(){
+    return {
+      users:[]
+    }
+  },
+  mounted(){
+    axios.get('http://localhost:3000/api/users').then(res=>{
+      this.users = res.data;
+    })
   }
 }
 </script>
